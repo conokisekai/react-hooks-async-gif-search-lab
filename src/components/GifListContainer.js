1ae111f6680bd8b35503cc0a48065b51979b57af
Conrad Kambi <kambiconrad@gmail.com>
2023-11-03 13:26:44 +0300

    Completed

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import GifList from "./GifList";
+import GifSearch from "./GifSearch";
+
+class GifListContainer extends Component {
+  state = {
+    gifs: []
+  };
+
+  handleSearch = (query) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=I9LQvL30976V3FKHoXHkfpGAQD6I1fVQ&rating=g`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ gifs: data.data.slice(0, 3) }); // Update state with the first 3 gifs
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <GifSearch onSearch={this.handleSearch} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    );
+  }
+}
+
+export default GifListContainer;
---
+++ src/components/GifSearch.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class GifSearch extends Component {
+  state = {
+    query: ""
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSearch(this.state.query);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={(e) => this.setState({ query: e.target.value })}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
+  }
+}
+
+export default GifSearch;
